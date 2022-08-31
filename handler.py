dfde10a51c00c55c2b2dc655d0568a57a808b783
CatchUp <mhanania@cohasa.cl>
2022-08-31 17:32:45 -0400

    first commit

---
+++ handler.py
@@ -0,0 +1,9 @@
+import json
+
+def ia(event, context):
+    body = {
+        "message": "Go Serverless v2.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
